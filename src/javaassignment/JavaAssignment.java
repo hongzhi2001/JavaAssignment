8d000ccdab89106d65655c4a5b54df152b379b4f
cs816 <hchingsiang@gmail.com>
2021-11-17 15:20:30 +0800

    test

--- src/javaassignment/JavaAssignment.java
+++ src/javaassignment/JavaAssignment.java
@@ -3,7 +3,7 @@ package javaassignment;
 public class JavaAssignment {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello world");  
     }
     
 }
